603eef3c26bc83096f415dc83d844e7818045d4f
unknown <you@example.com>
2018-12-04 10:24:07 +0700

    practical

---
+++ BaithiAsp/BaithiAsp/Context/ExampContext.cs
@@ -0,0 +1,14 @@
+﻿using BaithiAsp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BaithiAsp.Context
+{
+    public class ExampContext : DbContext
+    {
+        public DbSet<Examp> Examps { get; set; }
+    }
+}
---
+++ BaithiAsp/BaithiAsp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BaithiAsp.MvcApplication" Language="C#" %>
